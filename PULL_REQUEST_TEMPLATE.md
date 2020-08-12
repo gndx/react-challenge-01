e1920f4e9411319d9b10dd95c37f9fb28c456d8d
Oscar Barajas <oscar@arepa.dev>
2020-08-12 16:48:12 -0500

    First Commit

---
+++ PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,12 @@
+## DESCRIPTION
+
+Solución al reto 01 de React
+
+Nombre:
+Usuario Platzi:
+
+## Reto:
+  - [ ] Primer problema
+  - [ ] Segundo problema
+  - [ ] Tercer problema
+  - [ ] Cuarto Problema (Opcional)
---
+++ src/containers/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MapContainer from "../components/MapContainer";
+import '../styles/containers/App.styl';
+
+const App = () => {
+  return (
+    <div className="App">
+      <MapContainer />
+    </div>
+  )
+};
+
+export default App;
