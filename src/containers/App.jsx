a2e124918f36cd03b202bc774b711d6518722d16
Andres Loaiza <chelinandres@gmail.com>
2020-08-13 10:37:13 -0500

    ADD-solucion-challenger

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+/* eslint-disable react/jsx-no-undef */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import React,{useState,useEffect} from 'react';
 import MapContainer from "../components/MapContainer";
 import '../styles/containers/App.styl';
 
 const App = () => {
+  const [videos,setvideo]=useState({mylist: [], trends: [], originals: []});
+
+useEffect(()=>{
+  
+     fetch('http://localhost:3000/initalState')
+    .then(response=>response.json())
+    .then(data=>setvideo(data))
+},[])
   return (
     <div className="App">
-      <MapContainer />
-    </div>
+      {videos?(
+        <MapContainer videos={videos} />
+      ):null}
+    </div> 
+    
   )
 };
 
