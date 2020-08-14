ae7724ace78b5d52611ccf6a7ae2f5529297c7a5
“Jhoancanchila” <canchila.jhoan@gmail.com>
2020-08-13 20:07:37 -0500

    [ADD]-solution first problem

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import '../styles/containers/Button.css';
 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
+class MapContainer extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      show: false,
+    }
+    this.handleViewMap=()=>{
+      this.setState({show:!this.state.show})
+    }
+  }
+  
+  render () {
+    if(this.state.show){
+        return (
+        <>
+            <button className='style__button-hide' type='button' onClick={this.handleViewMap}>OCULTAR MAPA</button>
+          <Map
+              google={this.props.google}
+              zoom={5}
+              initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+            >
+              <Marker
+                position={{ lat: 19.4267261, lng: -99.1718706 }}
+              />
+          </Map>
+        </>
+        );
+    }else {
+      return(
+        <button className='style__button-show' type='button' onClick={this.handleViewMap}>MOSTRAR MAPA</button>
+      );
+    }
+  }
 }
 
 export default GoogleApiWrapper({
