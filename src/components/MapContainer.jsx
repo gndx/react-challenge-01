a33e615b57f9593be0bd4914d3596c9861b458bb
“Jhoancanchila” <canchila.jhoan@gmail.com>
2020-08-13 22:49:34 -0500

    [ADD]solution second problem

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -4,34 +4,41 @@ import '../styles/containers/Button.css';
 
 class MapContainer extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       show: false,
     }
-    this.handleViewMap=()=>{
-      this.setState({show:!this.state.show})
+    this.handleViewMap = () => {
+      this.setState({ show: !this.state.show })
     }
   }
-  
-  render () {
-    if(this.state.show){
-        return (
+
+  render() {
+    if (!this.state.show) {
+      return (
         <>
-            <button className='style__button-hide' type='button' onClick={this.handleViewMap}>OCULTAR MAPA</button>
+          <button className='style__button-hide' type='button' onClick={this.handleViewMap}>OCULTAR MAPA</button>
           <Map
-              google={this.props.google}
-              zoom={5}
-              initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-            >
-              <Marker
-                position={{ lat: 19.4267261, lng: -99.1718706 }}
-              />
+            google={this.props.google}
+            zoom={5}
+            initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+          >
+            {
+              this.props.markers.map(marker => (
+                  <Marker
+                    position={{ lat: marker.venueLat, lng: marker.venueLon }}
+                    key={marker.id}
+                    name={marker.venueName}
+                  />
+                )
+              )
+            }
           </Map>
         </>
-        );
-    }else {
-      return(
+      );
+    } else {
+      return (
         <button className='style__button-show' type='button' onClick={this.handleViewMap}>MOSTRAR MAPA</button>
       );
     }
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import MapContainer from "../components/MapContainer";
 import '../styles/containers/App.styl';
 
 const App = () => {
+  const API ='http://localhost:3000/locations';
+  const [markers, setMarkers]=useState([]);
+  
+  useEffect(() => {
+    fetch(API)
+      .then(response => response.json())
+      .then(data => { setMarkers(data)})
+  }, [])
   return (
+    
     <div className="App">
-      <MapContainer />
+       <MapContainer markers={markers} />
     </div>
   )
 };
