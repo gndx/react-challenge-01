8685502ef94c5124cd69135ce0586a99ed773a4c
María Pérez <maria_erez@hotmail.es>
2020-08-17 21:52:02 -0500

    first problem: still working

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
+export const MapContainer= class extends React.PureComponent{
+  
+  state={
+   show: false,
+  }
+
+  render(){
+
+    const {google}= this.props;
+    [show, setShow] = useState(false);
+
+    return (
+      <>
+        <Map
+          google={google}
+          zoom={5}
+          initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+        >
+          <Marker
+            position={{ lat: 19.4267261, lng: -99.1718706 }}
+          />
+        </Map>
+        <button type='button'>Ocultar/ver mapa</button>
+      </>
+    );
+  }
 }
 
 export default GoogleApiWrapper({
