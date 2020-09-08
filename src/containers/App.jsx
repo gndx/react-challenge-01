be5ad4cd8295afd067b31cdeea52e4a25d41b4d2
eliza <elizabethcubides200616@gmail.com>
2020-09-08 00:12:56 -0500

    reto react

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React,{ useState, useEffect} from 'react';
 import MapContainer from "../components/MapContainer";
 import '../styles/containers/App.styl';
 
 const App = () => {
+  const [ locations, setlocations] = useState([]);
+
+  useEffect(() => {
+    const API = 'http://localhost:3000/locations';
+    window.fetch(API)
+    .then(reponse=>reponse.json())
+    .then(data => setlocations(data));
+  },[])
   return (
     <div className="App">
-      <MapContainer />
+      <MapContainer locations={locations} />
     </div>
   )
 };
