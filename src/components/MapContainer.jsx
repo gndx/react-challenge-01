f7a2f1284d5e20e08d00636da7e63352eb027583
Sebastián Carabali <sebastiancc@unicauca.edu.co>
2020-09-09 16:22:24 -0500

    Se cambia el componente MapContainer a clase y se agrega boton mostrar/ocultar mapa

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,20 +1,58 @@
 import React from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import './MapContainer.styl';
 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
+class MapContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+    };
+    this.handleShowClick = this.handleShowClick.bind(this);
+  }
+
+  handleShowClick() {
+    this.setState(state => ({
+      show: !state.show,
+    }));
+  }
+
+  render() {
+    const { show } = this.state;
+    const { google } = this.props;
+    return (
+      <>
+        {show && (
+          <Map
+            google={google}
+            zoom={5}
+            initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+          >
+            <Marker position={{ lat: 19.4267261, lng: -99.1718706 }} />
+          </Map>
+        )}
+        {show ? (
+          <button
+            type="button"
+            onClick={this.handleShowClick}
+            className="btn-show-hide btn-hide"
+          >
+            Ocultar mapa
+          </button>
+        ) : (
+          <button
+            type="button"
+            onClick={this.handleShowClick}
+            className="btn-show-hide btn-show"
+          >
+            Mostrar mapa
+          </button>
+        )}
+      </>
+    );
+  }
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw'
+  apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw',
 })(MapContainer);
