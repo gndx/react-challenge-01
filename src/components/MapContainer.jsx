36eb01b2978a653762c048ec3fc5c4f29f35e004
Alfonso Neil Jimenez Casallas <alfonso@gradiweb.com>
2020-09-20 18:26:36 -0500

    Solución al segundo problema del reto: Agregar otro Marker para Platzi HQ Bogota en el mapa.

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -25,7 +25,14 @@ class MapContainer extends Component {
                            initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
                         >
                            <Marker
+						      title={'Platzi HQ CDMX'}
+						      name={'Platzi HQ CDMX'}
                               position={{ lat: 19.4267261, lng: -99.1718706 }}
+                           />
+						   <Marker
+						      title={'Platzi HQ Bogota'}
+						      name={'Platzi HQ Bogota'}
+                              position={{ lat: 4.6560716, lng: -74.0595918 }}
                            />
                         </Map>
 						</>
