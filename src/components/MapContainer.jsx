06fca273aadae73eacb093c84bd995a3a9e65370
Yoan Malaver <yoanmalaver@gmail.com>
2020-10-06 03:06:33 -0500

    Firts Problem Solved Class and Button

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,20 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import '../styles/containers/Button.css';
 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
+class MapContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+    };
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({ show: !prevState.show }));
+  };
+
+  render() {
+    //destruturación
+    const { show } = this.state;
+    const { google } = this.props;
+    switch (show) {
+      case true:
+        return (
+          <>
+            <button
+              type="button"
+              onClick={this.handleClick}
+              className="style__button-show"
+            >
+              Ocultar Mapa
+            </button>
+            <Map
+              google={google}
+              zoom={5}
+              initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+            >
+              <Marker position={{ lat: 19.4267261, lng: -99.1718706 }} />
+            </Map>
+          </>
+        );
+      default:
+        return (
+          <button
+            type="button"
+            onClick={this.handleClick}
+            className="style__button-hide"
+          >
+            Mostrar Mapa
+          </button>
+        );
+    }
+  }
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw'
+  apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw',
 })(MapContainer);
