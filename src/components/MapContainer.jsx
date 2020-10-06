17d8bfd5023e1c11339a2d4d795ecd65d77dda62
adriana_ardila <adriana2014lucia@outlook.com>
2020-10-06 01:35:03 -0500

    Challenge Completed

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,20 +1,51 @@
 import React from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
-}
+class MapContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+    };
+    this.showMap = this.showMap.bind(this);
+  }
 
+  showMap() {
+    if (this.state.show) {
+      this.setState({ show: false });
+    } else {
+      this.setState({ show: true });
+    }
+  }
+
+  render() {
+    if (this.state.show) {
+      return (
+        <React.Fragment>
+          <button type="button" className="map__button" onClick={this.showMap}>
+            Hide map
+          </button>
+          <Map
+            style={{ height: '90vh' }}
+            google={this.props.google}
+            zoom={5}
+            initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+          >
+            {this.props.markers.map(element => (
+              <Marker
+                key={element.id}
+                name={element.venueName}
+                position={{ lat: element.venueLat, lng: element.venueLon }}
+              />
+            ))}
+          </Map>
+        </React.Fragment>
+      );
+    } else {
+      return <button onClick={this.showMap}>Show map</button>;
+    }
+  }
+}
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw'
+  apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw',
 })(MapContainer);
