7d26932595880817aeb8b4e32cf071e0eb71bf24
Juan Luis Chavez <juan_luis_xw@hotmail.com>
2020-10-09 14:15:10 -0500

    Se soluciona primer y segundo Problema

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-const MapContainer = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={5}
-      initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
-    >
-      <Marker
-        position={{ lat: 19.4267261, lng: -99.1718706 }}
-      />
-    </Map>
-  );
+class MapContainer extends Component {
+  constructor(props){
+		super(props);
+		this.state = {
+			showMap: false,
+    };
+  }
+
+  mostrarMapa = () => {
+    this.setState({showMap: !this.state.showMap});
+  }
+
+  render() {
+    const { showMap } = this.state;
+    console.log(`Variable ${showMap}`);
+      return (
+        <>
+          <button onClick={this.mostrarMapa}>{showMap ? 'Ocultar Mapa' : 'Mostrar Mapa'}</button>
+          {showMap ? (
+            <Map
+              google={google}
+              zoom={5}
+              initialCenter={{ lat: 19.5943885, lng: -97.9526044 }}
+            >
+              <Marker
+                position={{ lat: 19.4267261, lng: -99.1718706 }}
+              />
+              <Marker
+                position={{ lat: 4.6560716, lng: -74.0595918 }}
+                title="Platzi HQ Bogota"
+                name="SOMA"
+              />
+            </Map>
+                )
+            :
+            <h1>Nooo Existe</h1>
+          }
+        </>
+          )
+  }
 }
 
 export default GoogleApiWrapper({
